54893be123b08e33408c2cc7a619a597aa82d6f5
Gajalakshmi <gajoo96@gmail.com>
2023-08-18 08:56:49 +0530

    QA code

--- src/org/finalgit/GitConflict.java
+++ src/org/finalgit/GitConflict.java
@@ -4,7 +4,8 @@ public class GitConflict {
 
 	public static void main(String[] args) {
 		System.out.println("To check on final git");
-
+		System.out.println("Testers Code");
+		System.out.println("Testers code working fine in QA");
 	}
 
 }
